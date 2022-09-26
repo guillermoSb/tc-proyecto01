53b757b0e674111f7fa8111292fea32c6d8f2202
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-09-26 10:22:40 -0600

    bugfix

--- test_proyecto1.py
+++ test_proyecto1.py
@@ -221,6 +221,6 @@ def test_can_simulate_afn_4():
     regex = Regex("(b|b)*@a@b@b@(a|b)*")
     automataFromRegex = Automata.fromRegex(regex)
     # Act
-    result = automataFromRegex.simulate_afn("babbaaaaa")
+    result = automataFromRegex.simulate_afd("babbaaaaa")
     # Assert
     assert result
