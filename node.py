ac215d54c2014755da4827295397bcd4f5817448
Guillermo Santos <guillermo.santosba@gmail.com>
2022-09-28 18:32:59 -0600

    progress

--- node.py
+++ node.py
@@ -1,17 +1,22 @@
 class Node:
     first_pos = []
     last_pos = []
-    def __init__(self, left_child, right_child,value, middle_child = None, position = None):
+    follow_pos = {
+
+    }
+    def __init__(self, left_child, right_child,value, middle_child = None, position = None, root = None):
         # In the case that the node only has a middle child assign a boolean to make the calculations easier
         if middle_child is not None:
             self.single_child = True
             self.middle_child = middle_child
         else:
             self.single_child = False
+            self.middle_child = None
             self.left_child = left_child
             self.right_child = right_child
         self.value = value
         self.position = position
+        self.root = root
 
 
     def nullable(self):
@@ -23,3 +28,5 @@ class Node:
         # It is a concatenation and all the children are null
         if self.value == "@" and self.left_child.nullable() and self.right_child.nullable(): return True
         return False
+
+
--- regex.py
+++ regex.py
@@ -64,6 +64,8 @@ class Regex:
                         rightOperand.last_pos = [position]
                         position += 1
                     new_node = Node(left_child=leftOperand, right_child=rightOperand, value=item)
+                    leftOperand.root = new_node
+
                     # If the operator is an union then the first pos is the union of the two childs
                     if item == "|":
                         new_node.first_pos = leftOperand.first_pos + rightOperand.first_pos
@@ -75,6 +77,8 @@ class Regex:
                             new_node.first_pos = new_node.first_pos + rightOperand.first_pos
                         if rightOperand.nullable():
                             new_node.last_pos = new_node.last_pos +  leftOperand.last_pos
+
+
                 else:
                     # It is a * only has one middle child
                     operand = tree_stack.pop(0)
@@ -85,7 +89,41 @@ class Regex:
                     new_node = Node(middle_child=operand,value=item, left_child=None, right_child=None)
                     new_node.first_pos = operand.first_pos
                     new_node.last_pos = operand.last_pos
+
+
                 # Add the new node to the tree stack
                 tree_stack.insert(0, new_node)
+
+        curr_node = tree_stack[0]
+        follow_pos = {}
+        visit_queue = [curr_node.left_child]
+        visited = []
+        for i in range(1, position + 1):
+            follow_pos[str(i)] = []
+
+        while len(visit_queue) != 0:
+            visit_node = visit_queue.pop()
+            if visit_node in visited:
+                continue
+
+            if visit_node.middle_child is None:
+                if visit_node.left_child is not None and visit_node.left_child.value == "@":
+                    for fp in visit_node.left_child.last_pos:
+                        for lp in visit_node.right_child.first_pos:
+                            follow_pos[str(fp)].append(lp)
+            if visit_node.middle_child is not None:
+                if visit_node.value == "*":
+                    for fp in visit_node.last_pos:
+                        for lp in visit_node.first_pos:
+                            follow_pos[str(fp)].append(lp)
+
+            if visit_node.single_child and visit_node.middle_child is not None:
+                visit_queue.append(visit_node.middle_child)
+            elif not visit_node.single_child and visit_node.left_child is not None:
+                visit_queue.append(visit_node.left_child)
+                visit_queue.append(visit_node.right_child)
+            visited.append(visit_node)
+
+
         return tree_stack[0]
 
